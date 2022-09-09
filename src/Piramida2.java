4bc4dce1c396d67ff25547834e71979b22340eff
JatiWahyuKusuma <jatiikusuma@gmail.com>
2022-09-09 12:49:24 +0700

    perbaikan

--- src/Piramida2.java
+++ src/Piramida2.java
@@ -2,6 +2,7 @@ public class Piramida2 {
    public static void main(String args[]) {
         int[] nim = { 2, 1, 4, 1, 7, 6, 2, 1, 3, 9};
         int index = 0;
+        System.out.println("Nim : 2141762139");
         for (int i : nim) {
             index = index + i;
         }
--- src/PiramidaNim.java
+++ src/PiramidaNim.java
@@ -3,12 +3,12 @@ public class PiramidaNim {
     public static void main(String[] args) throws Exception {
         System.out.println("Nim : 2141762139");
         int[] nim = {2, 1, 4, 1, 7, 6, 2, 1, 3, 9 };
-        int h = 0, i = 5, j = 1;
+        int h = 0, i = 4, j = 1;
         while (j <= i) {
         int a = i, b = 1;
         while (a > j) {
         System.out.print(" ");
-        j--;
+        a--;
         }
         while (b <= j) {
             if (h > 9) {
